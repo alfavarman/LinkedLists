2f40c7a654f7afb0283b23a1ae9f058d0d6eafdc
CKS <communicate2charles@gmail.com>
2022-11-30 15:11:52 +0700

    print linked list

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 class Node:
-    def __init__(self, data, next):
+    def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
 
 
 class LinkedList:
-    def __int__(self):
+    def __init__(self):
         self.head = None
 
+    def insert_at_beginning(self, data):
+        node = Node(data=data, next=self.head)
+        self.head = node
+
+    def print_ll(self):
+        if self.head is None:
+            print("No Elements")
+            return
+
+        itr = self.head
+        while itr:
+            print(str(itr.data) + '---->', end=' ')
+            itr = itr.next
 
 
 if __name__ == '__main__':
     ll = LinkedList()
+    ll.insert_at_beginning(5)
+    ll.insert_at_beginning(33)
+    ll.insert_at_beginning(63)
+    ll.insert_at_beginning(8)
+    ll.print_ll()
+
 
 
