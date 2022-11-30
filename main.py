3508c0d9d0838f7389b28b20c0f989450c4a0a47
CKS <communicate2charles@gmail.com>
2022-12-01 00:40:37 +0700

    del index

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert_at_beginning(self, data):
+    def insert_at_beginning(self, data) -> None:
         node = Node(data=data, next=self.head)
         self.head = node
 
-    def print_ll(self):
+    def print_ll(self) -> None:
         if self.head is None:
             print("No Elements")
             return
@@ -21,14 +21,77 @@ class LinkedList:
         while itr:
             print(str(itr.data) + '---->', end=' ')
             itr = itr.next
+        print()
+
+    def insert_at_end(self, data) -> None:
+        # if the LL is empty:
+        if self.head is None:
+            self.head = Node(data=data, next=None)
+        else:
+            itr = self.head
+            while itr and itr.next is not None:
+                itr = itr.next
+            itr.next = Node(data=data, next=None)
+
+    def insert_values(self, arr: list) -> None:
+        for value in arr:
+            self.insert_at_end(value)
+
+    def len_ll(self) -> int:
+        if self.head is None:
+            return 0
+        counter = 1
+        itr = self.head
+        while itr and itr.next is not None:
+            itr = itr.next
+            counter += 1
+        return counter
+
+    def remove_index(self, idx: int):
+        if idx < 0 or idx >= self.len_ll():
+            raise Exception("index out of range")
+
+        if idx == 0:
+            self.head = self.head.next
+            return
+
+        counter = 0
+        itr = self.head
+        while itr and counter < idx -1:
+            counter += 1
+            itr = itr.next
+        itr.next = itr.next.next
+
+    def remove_value(self, val):
+        pass
+
+    def insert_at_index(self, idx: int, data: object):
+        pass
+
+    def insert_after_value(self, data_after: object, data_to_insert: object):
+        pass
+
 
 
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.insert_at_beginning(5)
+    ll.print_ll()
+    print(f'll.len(): {ll.len_ll()}')
+    ll.insert_values([22, 33, 44, 34, 36, 76])
+    ll.print_ll()
+    ll.remove_index(3)
+    ll.print_ll()
+    print(f'll.len(): {ll.len_ll()}')
     ll.insert_at_beginning(33)
+    ll.print_ll()
     ll.insert_at_beginning(63)
+    ll.print_ll()
     ll.insert_at_beginning(8)
+    ll.print_ll()
+    ll.insert_at_end(22.2)
+    ll.print_ll()
+    ll.insert_values([1, 22, 66, 77])
+    ll.print_ll()
     ll.print_ll()
 
 
