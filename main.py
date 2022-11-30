d2c28ed7d6a576c0081fe537c432d89087c87096
CKS <communicate2charles@gmail.com>
2022-11-30 14:35:27 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, data, next):
+        self.data = data
+        self.next = next
+
+
+class LinkedList:
+    def __int__(self):
+        self.head = None
+
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+
+
